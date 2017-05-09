927e4a58fb7d25add06eee26b7d9420c103f9f58
kedpak <kedpak@yahoo.com>
2017-05-09 20:40:33 +0000

    included len() function for loop iteration

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,8 @@
 def element_at(my_list, idx):
     if idx > len(my_list):
         return (None)
+    if idx < 0:
+        return (None)
     for i in my_list:
-        if i == my_list[idx]:
+        if i == len(my_list):
             return my_list[idx]
