dd859689b5ce9cff8fa021007fdd393c1d5a1827
kedpak <kedpak@yahoo.com>
2017-05-09 21:01:38 +0000

    added condition if idx is less than 0

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,8 @@
 def replace_in_list(my_list, idx, element):
     if idx >= len(my_list):
         return (my_list)
+    if idx < 0:
+        return (my_list)
     for i in my_list:
         if i == len(my_list):
             my_list[idx] = element
