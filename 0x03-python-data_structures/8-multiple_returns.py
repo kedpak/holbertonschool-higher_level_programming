0654e1a2bccc34e509922bd7050a7c142de26234
kedpak <kedpak@yahoo.com>
2017-05-10 03:00:19 +0000

    modified if condition if sentence is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 def multiple_returns(sentence):
     if sentence == "":
-        sentence[0] = None
-        return
-    t = len(sentence), sentence[0]
+        t = 0, None
+    else:
+        t = len(sentence), sentence[0]
     return (t)
