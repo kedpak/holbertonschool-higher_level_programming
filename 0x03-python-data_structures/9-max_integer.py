48fa41c7dedae953511fe9da17fb2fbdc730430f
kedpak <kedpak@yahoo.com>
2017-05-10 03:23:16 +0000

    fixed none condition

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if my_list == []:
         return None
-    largest = 0
+    largest = None
     for i in my_list:
         if i > largest:
             largest = i
