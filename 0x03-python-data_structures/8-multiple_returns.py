8a4b4d353466b3613457575d2c602c0cc3092964
kedpak <kedpak@yahoo.com>
2017-05-10 03:04:35 +0000

    added parentheses

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 def multiple_returns(sentence):
     if sentence == "":
-        t = 0, None
+        t = (0, None)
     else:
-        t = len(sentence), sentence[0]
+        t = (len(sentence), sentence[0])
     return (t)
