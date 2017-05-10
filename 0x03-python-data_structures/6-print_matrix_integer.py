7a5171e0001c02fdfc536fa4ab70c1319153a55b
kedpak <kedpak@yahoo.com>
2017-05-10 06:51:15 +0000

    changed positioning of none conditions

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,12 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
         print("")
-    if matrix is None:
-        print("")
-    for i in range(0, len(matrix)):
-        m = 1
-        for j in matrix[i]:
-            if m < len(matrix):
-                print(str.format('{:d}', j), end=" ")
-            elif m == len(matrix):
-                print(str.format('{:d}', j))
-            m += 1
+    if matrix is not None:
+        for i in range(0, len(matrix)):
+            m = 1
+            for j in matrix[i]:
+                if m < len(matrix):
+                    print(str.format('{:d}', j), end=" ")
+                elif m == len(matrix):
+                    print(str.format('{:d}', j))
+                m += 1
