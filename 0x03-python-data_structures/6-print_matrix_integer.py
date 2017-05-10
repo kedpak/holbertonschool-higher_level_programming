d33d300c0273e6821e7fc1004e2869b7adca7e4d
kedpak <kedpak@yahoo.com>
2017-05-10 01:50:14 +0000

    removed join function and implemented nested loop

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        row = ' '.join(str(c) for c in row if str(c) not in '[],')
-        print(str.format('{:}', row))
+    for i in range(0, len(matrix)):
+        for j in matrix[i]:
+            print(str.format('{:d}', j), end=" ")
+        print("")
