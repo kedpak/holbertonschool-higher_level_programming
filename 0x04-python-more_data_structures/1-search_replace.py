675703655385aeeb42392c8aecd1523f8139c42c
kedpak <kedpak@yahoo.com>
2017-05-12 06:23:28 +0000

    added empty condition

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is None:
+    if my_list is None or my_list == []:
         return None
     new_list = []
     i = 0
