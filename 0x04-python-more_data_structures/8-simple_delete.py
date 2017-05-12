48e708e5010af6a819d2b20a299c283eff168d08
kedpak <kedpak@yahoo.com>
2017-05-12 01:16:46 +0000

    first commit

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def simple_delete(my_dict, key=""):
+    switch = True  # need a switch to tell if found matching key
+    for i in my_dict:  # will delete more than needed without switch
+        if key == i:
+            switch = True
+            break
+        else:
+            switch = False
+    if switch is True:
+        my_dict.pop(i)
+    return (my_dict)
