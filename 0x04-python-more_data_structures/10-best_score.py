7381c004fcb1ba0e081d8f2ef99031dd41e95a6d
kedpak <kedpak@yahoo.com>
2017-05-12 04:50:33 +0000

    changed function to max

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,8 +2,4 @@
 def best_score(my_dict):
     if my_dict is None:
         return (None)
-    temp = 0
-    for i, j in my_dict.items():
-        if j > temp:
-            temp = j
-    return (temp)
+    return max(my_dict)
