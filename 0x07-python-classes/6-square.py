07e09ccd4be6171eba38dec9c2344b5ba07ec8ae
kedpak <kedpak@yahoo.com>
2017-05-23 02:01:54 +0000

    first commit

---
+++ 0x07-python-classes/6-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+class Square():
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return (self.__size**2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        for k in range(self.__position[1]):
+            if self.__position[1] > 0:
+                print(" ", end="")
+            print("")
+        for i in range(self.__size):
+            for m in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
