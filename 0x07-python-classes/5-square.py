2a64b49e71d40457a9f2b8a4e4df7338578ed7b1
kedpak <kedpak@yahoo.com>
2017-05-23 02:04:55 +0000

    fixed pep8

--- 0x07-python-classes/5-square.py
+++ 0x07-python-classes/5-square.py
@@ -6,11 +6,14 @@ class Square():
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         return (self.__size**2)
+
     @property
     def size(self):
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if type(value) != int:
@@ -18,6 +21,7 @@ class Square():
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def my_print(self):
         if self.__size == 0:
             print("")
