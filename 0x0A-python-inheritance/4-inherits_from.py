66aa6a505c276d42c34bbf1c0c4404222749201f
kedpak <kedpak@yahoo.com>
2017-05-31 04:41:58 +0000

    added documentation

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+'''module: 3-is_kind_of_class
+'''
+
+
 def inherits_from(obj, a_class):
+    '''method: inherits_from
+    '''
     if (issubclass(a_class, type(obj))):
         return (False)
     else:
