2adef25e9c0cd791b739e74d6b48cf518bc7531a
kedpak <kedpak@yahoo.com>
2017-05-31 04:23:27 +0000

    added documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+'''module: 2-is_same_class
+'''
+
+
 def is_same_class(obj, a_class):
+    '''method: is_same_class
+    '''
     if type(obj) is a_class:
         return (True)
     else:
