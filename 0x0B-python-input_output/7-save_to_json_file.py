be5299f520ae329d154b9c72c7ed6a01d158b485
kedpak <kedpak@yahoo.com>
2017-06-02 03:36:53 +0000

    first commit

---
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import json
+'''module: 7-save_to_json_file
+'''
+
+def save_to_json_file(my_obj, filename):
+    '''method: save_to_json_file
+    method writes an object to text file, using
+    JSON representation
+    '''
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
