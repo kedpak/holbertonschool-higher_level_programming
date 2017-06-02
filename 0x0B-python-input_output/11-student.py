4f496b6226b9c459e28fda8d3b95dca4b586b152
kedpak <kedpak@yahoo.com>
2017-06-02 22:59:01 +0000

    first commit

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''module: 11-student
+'''
+
+
+class Student:
+    '''class: Student
+    defines student by first name, last name, and age
+    '''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return(self.__dict__)
