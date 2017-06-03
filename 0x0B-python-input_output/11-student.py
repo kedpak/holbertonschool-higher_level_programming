703521df8579b8813b66ba50124c9045a79256d7
kedpak <kedpak@yahoo.com>
2017-06-03 05:46:50 +0000

    added documentation

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -8,9 +8,13 @@ class Student:
     defines student by first name, last name, and age
     '''
     def __init__(self, first_name, last_name, age):
+        '''constructor
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        '''method: return dictionary representatin of attributes
+        '''
         return(self.__dict__)
