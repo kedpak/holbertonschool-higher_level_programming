55e509a66f08c32d419e5f2d8a8a2ba2cd3ec9b3
kedpak <kedpak@yahoo.com>
2017-06-04 00:44:44 +0000

    implemented a loop through json dict, got rid of hard code

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -8,11 +8,15 @@ class Student:
     defines student by first name, last name, and age
     '''
     def __init__(self, first_name, last_name, age):
+        '''constructor
+        '''
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        '''method: replace attribute with passed in attrivtue
+        '''
         new_dict = {}
         if attrs is None:
             return(self.__dict__)
