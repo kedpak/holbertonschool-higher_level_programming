b1c8b222388a0bebda8de1a4b16e6ebb74c2d6fb
kedpak <kedpak@yahoo.com>
2017-07-13 21:08:16 +0000

    removed database name

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- script creates table called first_table in current database
 -- adds table to the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (
+CREATE TABLE IF NOT EXISTS first_table (
        id INT NOT NULL AUTO_INCREMENT,
        name VARCHAR(256) NOT NULL,
        PRIMARY KEY(id)
