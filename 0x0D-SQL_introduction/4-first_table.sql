2323641a155d908d8ecb66d8e9542944dcdd8cd6
kedpak <kedpak@yahoo.com>
2017-07-13 20:47:04 +0000

    added a USE to set hbtn to current database

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,7 @@
 -- script creates table called first_table in current database
+-- sets hbtn_0c_0 as current database
+USE `hbtn_0c_0`
 -- adds table to the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (
+CREATE TABLE IF NOT EXISTS SELECT hbtn_0c_0.first_table (
        id INT, name VARCHAR(256)
 );
