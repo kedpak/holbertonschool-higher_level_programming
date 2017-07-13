758bf0e78a66a99bb709a3a842f941744576e191
kedpak <kedpak@yahoo.com>
2017-07-13 21:02:46 +0000

    added NOT NULL and AUTO_INCREMENT

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- script creates table called first_table in current database
 -- adds table to the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (
-       id INT,
+       id INT NOT NULL AUTO_INCREMENT,
        name VARCHAR(256),
        PRIMARY KEY(id)
 );
