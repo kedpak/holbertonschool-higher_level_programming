9fbcc458924e7f07b4a3a09d580555dc6e684c9b
kedpak <kedpak@yahoo.com>
2017-07-13 20:54:35 +0000

    fixed typo

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- script creates table called first_table in current database
 -- adds table to the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS SELECT hbtn_0c_0.first_table (
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (
        id INT,
        name VARCHAR(256),
        PRIMARY KEY(id)
