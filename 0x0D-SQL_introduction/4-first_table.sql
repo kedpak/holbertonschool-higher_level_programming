5e57d452d6908f0bc1dcfbd7e31337b79fd729c4
kedpak <kedpak@yahoo.com>
2017-07-13 21:03:21 +0000

    added NOT NULL to name

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,6 +2,6 @@
 -- adds table to the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (
        id INT NOT NULL AUTO_INCREMENT,
-       name VARCHAR(256),
+       name VARCHAR(256) NOT NULL,
        PRIMARY KEY(id)
 );
