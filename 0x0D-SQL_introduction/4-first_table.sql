a6d9fa03dd3532853e6d3109f115685ffc7a9cb8
kedpak <kedpak@yahoo.com>
2017-07-13 19:29:17 +0000

    first commit

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- script creates table called first_table in current database
+-- adds table to the database hbtn_0c_0
+CREATE TABLE IF NOT EXISTS hbtn_0c_0.first_table (
+       id INT, name VARCHAR(256)
+);
