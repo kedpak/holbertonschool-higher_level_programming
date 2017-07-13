7a8d6d93b608ccd19b685fd2cd15243fa668b6df
kedpak <kedpak@yahoo.com>
2017-07-13 20:53:24 +0000

    added PRIMARY KEY

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,7 @@
 -- script creates table called first_table in current database
--- sets hbtn_0c_0 as current database
-USE `hbtn_0c_0`
 -- adds table to the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS SELECT hbtn_0c_0.first_table (
-       id INT, name VARCHAR(256)
+       id INT,
+       name VARCHAR(256),
+       PRIMARY KEY(id)
 );
