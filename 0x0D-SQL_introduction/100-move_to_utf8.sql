6b64947a97a478c2ec9c07abfe1264af224882e1
kedpak <kedpak@yahoo.com>
2017-07-14 19:26:19 +0000

    first commit

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- script converts database to UTF8
+-- In database hbtn_0c_0 table first_table, convert field name to UTF8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
