47ae92fba8ff92f74de3a35e4eb2380021a8bb11
kedpak <kedpak@yahoo.com>
2017-07-14 01:03:30 +0000

    re-arranged order of list print out

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- script lists all records in table
 -- prints all the records in second_table in descending order
-SELECT * FROM second_table ORDER BY score DESC
+SELECT score, name FROM second_table ORDER BY score DESC
