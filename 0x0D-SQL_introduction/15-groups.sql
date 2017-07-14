e67d63a93dd2a8c7a9b5b240e9047422ce726732
kedpak <kedpak@yahoo.com>
2017-07-14 02:27:11 +0000

    first commit

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,4 @@
+-- script lists the number of records with same score on table
+-- counts number of each score occurence in second_table
+SELECT score, COUNT(*) as number FROM second_table
+       GROUP BY score, number ORDER BY number DESC
