3d69fe4214b8db1b045780de4d301ea7e85d9fda
kedpak <kedpak@yahoo.com>
2017-07-16 01:31:13 +0000

    removed redundant info

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,7 +1,6 @@
 -- Write a script that creates the table
 -- creates table force_name with id and name fields
 CREATE TABLE IF NOT EXISTS force_name (
-       id INT NOT NULL AUTO_INCREMENT,
+       id INT,
        name VARCHAR(256) NOT NULL,
-       PRIMARY KEY(id)
 );
