1232f8680f69a2d81216b60185935e1e145eb539
kedpak <kedpak@yahoo.com>
2017-07-16 02:08:13 +0000

    first commit

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,7 @@
+-- Write a script that creates the table
+-- table is created called id_not_null, id and name values
+CREATE TABLE IF NOT EXISTS id_not_null (
+       id INT DEFAULT 1,
+       name VARCHAR(256),
+       PRIMARY KEY(id)
+);
