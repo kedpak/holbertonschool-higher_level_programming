ffb6887f11d3e09cfb8cab1eb3156ea16ab70cc4
kedpak <kedpak@yahoo.com>
2017-07-16 02:13:31 +0000

    changed format and added 4

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,3 @@
 -- Write a script that creates the table
 -- creates table force_name with id and name fields
-CREATE TABLE IF NOT EXISTS force_name (
-       id INT,
-       name VARCHAR(256) NOT NULL,
-);
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
