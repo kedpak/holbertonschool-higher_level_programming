1095a649bf46f11299d859014fbd569c54c0e5c8
kedpak <kedpak@yahoo.com>
2017-07-17 01:38:17 +0000

    first commit

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,3 @@
+-- Write a script that lists all the cities of California
+-- Lists all fields with state_id of 1 (california)
+SELECT * FROM hbtn_0d_usa.cities  WHERE state_id = 1 ORDER BY cities.id ASC;
