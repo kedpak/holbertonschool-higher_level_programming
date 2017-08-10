5ee14bc374d3622ace87f773dbde460db286fe6b
kedpak <kedpak@yahoo.com>
2017-08-10 21:51:34 +0000

    added port which connects to 3306

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,6 +8,7 @@ import MySQLdb
 import sys
 
 db = MySQLdb.connect(host='localhost',
+                     port='3306',
                      user='root',
                      passwd='root',
                      db='hbtn_0e_0_usa')
