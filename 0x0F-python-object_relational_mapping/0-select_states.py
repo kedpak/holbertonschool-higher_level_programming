d0ccd6b99456bf115437a767bf137a6e64a37b11
kedpak <kedpak@yahoo.com>
2017-08-10 22:20:59 +0000

    changed user and passwd input be argv

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,8 +9,8 @@ import sys
 
 db = MySQLdb.connect(host="localhost",
                      port=3306,
-                     user="root",
-                     passwd="root",
+                     user=sys.argv[1],
+                     passwd=sys.argv[2],
                      db="hbtn_0e_0_usa",
                      charset="utf8")
 
