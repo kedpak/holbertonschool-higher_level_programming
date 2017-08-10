3a65861f3a095f749e1636cbc016e66a6163e71e
kedpak <kedpak@yahoo.com>
2017-08-10 21:59:34 +0000

    made file executable

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,12 @@ lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host='localhost',
-                     port='3306',
-                     user='root',
-                     passwd='root',
-                     db='hbtn_0e_0_usa')
+db = MySQLdb.connect(host="localhost",
+                     port=3306,
+                     user="root",
+                     passwd="",
+                     db="hbtn_0e_0_usa",
+                     charset="utf8")
 
 cursor = db.cursor()
 cursor.execute('SELECT * FROM hbtn_0e_0_usa.states')
