85917fffc77d3b4cd5f876a2ce41700afbe5fe27
kedpak <kedpak@yahoo.com>
2017-08-10 22:37:06 +0000

    added condition to prevent module running if exported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,19 +5,31 @@ lists all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
+
+
 import sys
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3],
-                     charset="utf8")
 
-cursor = db.cursor()
-cursor.execute('SELECT * FROM hbtn_0e_0_usa.states ORDER BY states.id')
-result = cursor.fetchall()
-for i in result:
-    print(i)
-cursor.close()
-db.close()
+def list_states():
+    """
+    method lists all states from database
+    """
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         charset="utf8")
+
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM hbtn_0e_0_usa.states ORDER BY states.id')
+    result = cursor.fetchall()
+
+    for i in result:
+        print(i)
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    list_states()
