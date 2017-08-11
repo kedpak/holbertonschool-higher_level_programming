687f133a1291ab560dd1749b6bf4479bf7c84526
kedpak <kedpak@yahoo.com>
2017-08-11 23:22:37 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+creates a class State which connects to states table
+"""
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    links to states table with class attributes id and name
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
