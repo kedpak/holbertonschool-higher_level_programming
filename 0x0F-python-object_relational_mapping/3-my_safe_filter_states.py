36fd4bf9d1c9789345444d6f8c3577bd022499a3
kedpak <kedpak@yahoo.com>
2017-08-11 00:42:43 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+module select_states:
+lists all states passed into argument
+prevent sql injection
+"""
+
+import MySQLdb
+import sys
+import string
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    query = """SELECT * FROM states WHERE
+    name LIKE '{0}' ORDER BY id ASC;"""
+    cursor = db.cursor()
+    cursor.execute(query.format(sys.argv[4]))
+    result = cursor.fetchall()
+
+    for i in result:
+        print(i)
+    cursor.close()
+    db.close()
