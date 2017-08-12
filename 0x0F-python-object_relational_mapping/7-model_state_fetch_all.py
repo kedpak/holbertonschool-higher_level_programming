2260650cd1756fa420e2a21964b0deaeb57116d0
kedpak <kedpak@yahoo.com>
2017-08-12 00:21:12 +0000

    removed metadata

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,7 +14,6 @@ if __name__ == "__main__":
     engine = sqlalchemy.create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
             sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
 
     Session = sessionmaker(engine)
     session = Session()
