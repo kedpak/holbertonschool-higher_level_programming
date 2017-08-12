a3b27210c1ef67dc45756ff107b5facd2fc359b0
kedpak <kedpak@yahoo.com>
2017-08-12 04:00:54 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+add an object to the database
+"""
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+from model_state import State, Base
+
+if __name__ == "__main__":
+    engine = sqlalchemy.create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    new_state = State(name='Lousiana')
+    session.add(new_state)
+
+    print(new_state.id)
+    session.commit()
+    session.close()
