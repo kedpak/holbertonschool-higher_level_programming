3f735a27fcd654d30ba993ca25a215c262ac6625
kedpak <kedpak@yahoo.com>
2017-08-12 04:33:27 +0000

    first commit

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+delete all object containing 'a'
+"""
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import sys
+from model_state import State, Base
+
+if __name__ == "__main__":
+    engine = sqlalchemy.create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    del_sess = session.query(State).filter(State.name.contains('a'))
+    del_sess.delete()
+
+    session.commit()
+    session.close()
