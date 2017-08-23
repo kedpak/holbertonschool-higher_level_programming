60662ec0d79de6f130d5c26e337e0d650aa0df1d
kedpak <kedpak@yahoo.com>
2017-08-23 03:56:01 +0000

    remove redundant flag and added data flag

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request to the passed URL, and displays body
-curl -L -X POST -d "email=hr@holbertonschool.com&subject=I+will+always+be+here+for+PLD" "$1"
+curl -X POST -sd "email=hr@holbertonschool.com&subject=I+will+always+be+here+for+PLD" "$1"
