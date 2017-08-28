425230193cd903930d15f1f6bf853294851c531e
kedpak <kedpak@yahoo.com>
2017-08-28 22:07:54 +0000

    first commit

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Python script that fetches URL
+prints out response, response type, and utf content of response
+"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+    print("Body response:")
+    print("\t - type: {}".format(type(html)))
+    print("\t - content: {}".format(html))
+    print("\t - utf8 content: {}".format(html.decode("utf8")))
