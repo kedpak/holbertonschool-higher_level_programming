c31ae48268e7a49a3090d604b7d0ea80936dfa26
kedpak <kedpak@yahoo.com>
2017-08-28 23:42:55 +0000

    first commit

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as parameter,
+displays the body of the response
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+
+    email = urllib.parse.urlencode(dict({'email':sys.argv[2]})).encode('utf-8')
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(req, email) as response:
+        body = response.read()
+        print(body.decode("utf-8"))
