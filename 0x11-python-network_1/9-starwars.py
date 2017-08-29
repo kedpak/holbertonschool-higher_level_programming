e73fe46703c627e878d4f0b2679f70060424b020
kedpak <kedpak@yahoo.com>
2017-08-29 06:37:43 +0000

    made change to add search end point, and got rid of loop for redundant code

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -8,22 +8,8 @@ import sys
 
 if __name__ == "__main__":
 
-    arr_names = []
-    i = 1
-
-    while (i < 89):
-        req = requests.get('https://swapi.co/api/people/' + str(i) + '/')
-        req = req.json()
-
-        if 'name' not in req:
-            """some id's from the api are empty, this conditons takes
-            care of empty dicts"""
-            i += 1
-            continue
-        if sys.argv[1].lower() in req['name'].lower():
-            arr_names.append(req['name'])
-        i += 1
-
-    print("Number of result: {}".format(len(arr_names)))
-    for v in arr_names:
-        print(v)
+    req = requests.get('https://swapi.co/api/people/?search=' + sys.argv[1])
+    req = req.json()
+    print("Number of result: {}".format(req['count']))
+    for name in req['results']:
+        print(name['name'])
