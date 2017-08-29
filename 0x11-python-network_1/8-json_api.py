bf17aa552e6943c8ed9a7d430e11b9eaf6aa4dde
kedpak <kedpak@yahoo.com>
2017-08-29 03:03:34 +0000

    add fail conditions

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,10 +9,18 @@ import sys
 
 if __name__ == "__main__":
 
-    url = "http://0.0.0.0:5000/search_user"
-    req = requests.post(url, data={'q' : sys.argv[1]})
-    data_str = req.content.decode("utf8")
-    data_str = data_str.replace('{', "").replace('}', "").replace(
-        '\n', "").replace(' ', "")
-    dic2 = dict(item.split(':') for item in data_str.split(','))
-    print("[{}] {}".format(dic2['"id"'], dic2['"name"'].strip('"')))
+    if len(sys.argv) != 2:
+        print("No Result")
+    elif sys.argv[1].isalpha() is False:
+        print("No Result")
+    else:
+        url = "http://0.0.0.0:5000/search_user"
+        req = requests.post(url, data={'q': sys.argv[1]})
+        if req.headers['content-type'] != 'application/json':
+            print("Not a valid JSON")
+        else:
+            data_str = req.content.decode("utf8")
+            data_str = data_str.replace('{', "").replace('}', "").replace(
+                '\n', "").replace(' ', "")
+            dic2 = dict(item.split(':') for item in data_str.split(','))
+            print("[{}] {}".format(dic2['"id"'], dic2['"name"'].strip('"')))
