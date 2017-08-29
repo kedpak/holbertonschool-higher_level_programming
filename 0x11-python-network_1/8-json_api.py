d2182339e46f77f1396d0203f2e863c0ddaea00e
kedpak <kedpak@yahoo.com>
2017-08-29 21:08:42 +0000

    replace print with stderr

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,14 +10,14 @@ import sys
 if __name__ == "__main__":
 
     if len(sys.argv) != 2:
-        print("No Result")
+        sys.stderr.write("No Result\n")
     elif sys.argv[1].isalpha() is False or sys.argv[1].islower() is False:
-        print("No Result")
+        sys.stderr.write("No Result\n")
     else:
         url = "http://0.0.0.0:5000/search_user"
         req = requests.post(url, data={'q': sys.argv[1]})
         if req.headers['content-type'] != 'application/json':
-            print("Not a valid JSON")
+            sys.stderr.write("Not a valid JSON\n")
         else:
             req = req.json()
             print("[{}] {}".format(req['id'], req['name']))
