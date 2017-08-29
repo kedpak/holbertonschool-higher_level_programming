727ce1fe6ffedaae48b2986a65897835103690e3
kedpak <kedpak@yahoo.com>
2017-08-29 06:53:42 +0000

    change decode method to text method

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
 
     req = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t - type: {}".format(type(req.content.decode("utf8"))))
-    print("\t - content: {}".format(req.content.decode("utf8")))
+    print("\t - type: {}".format(type(req.text)))
+    print("\t - content: {}".format(req.text))
