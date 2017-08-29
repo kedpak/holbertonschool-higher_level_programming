235fa97f903bb0c7c73ab276b82c876f29c0b871
kedpak <kedpak@yahoo.com>
2017-08-29 06:57:55 +0000

    change hyphen placement

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,8 +6,7 @@ Python script that fetches https://intranet.hbtn.io/status
 import requests
 
 if __name__ == "__main__":
-
     req = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
-    print("\t - type: {}".format(type(req.text)))
-    print("\t - content: {}".format(req.text))
+    print("\t- type: {}".format(type(req.content.decode("utf8"))))
+    print("\t- content: {}".format(req.content.decode("utf8")))
