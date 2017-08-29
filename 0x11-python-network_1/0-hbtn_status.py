2feb99cb05d852c58350bbd484125c88f31dd1ec
kedpak <kedpak@yahoo.com>
2017-08-29 06:58:32 +0000

    change hyphen placement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         html = response.read()
     print("Body response:")
-    print("\t - type: {}".format(type(html)))
-    print("\t - content: {}".format(html))
-    print("\t - utf8 content: {}".format(html.decode("utf8")))
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode("utf8")))
