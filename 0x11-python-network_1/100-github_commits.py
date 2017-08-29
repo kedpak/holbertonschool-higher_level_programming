f1448a67b38b903f4ad8eda6184e4704865749c2
kedpak <kedpak@yahoo.com>
2017-08-29 23:59:39 +0000

    first commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+10 commits (from the most recent to oldest) of
+repository rails by the user rails
+must use the Github API
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/" + sys.argv[
+        2] + "/" + sys.argv[1] + "/commits"
+    req = requests.get(url)
+    req = req.json()
+
+    i = 0
+    while (i < 10):
+
+        print("{}: {}".format(req[i]['sha'],
+                              req[i]['commit']['committer']['name']))
+        i += 1
