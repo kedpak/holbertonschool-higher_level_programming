23db40331ce1430dbcb2c86c81673e3be00c4d29
kedpak <kedpak@yahoo.com>
2017-08-29 06:12:01 +0000

    first commit

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Python script that takes your Github credentials
+(username and password) and uses the Github API to display your id
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+
+    url = 'https://api.github.com/user/'
+    userName = sys.argv[1]
+    userPswd = sys.argv[2]
+    req = requests.get(url, auth=(userName, userPswd))
+    req = req.json()
+    print(req.get('id'))
