99c063062802c4873ad5c066dc63225f70f1979f
kedpak <kedpak@yahoo.com>
2017-08-30 05:19:53 +0000

    first commit

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a string and sends
+a search request to the Star Wars API
+Manages pagination
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    name_list = []
+    req = requests.get('https://swapi.co/api/people/?search=' + sys.argv[1])
+    req = req.json()
+    print("Number of result: {}".format(req['count']))
+
+    i = 0
+    while i <= len(req['results']):
+        if req['next'] is not None and i == len(req['results']):
+            req2 = requests.get(req['next'])
+            req = req2.json()
+            i = 0
+        try:
+            name_list.append(req['results'][i].get('name'))
+        except:
+            pass
+        i += 1
+
+    for name in name_list:
+        print(name)
