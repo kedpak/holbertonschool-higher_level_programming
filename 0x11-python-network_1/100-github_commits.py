640e18388463f06177b766593248653fc71273ee
kedpak <kedpak@yahoo.com>
2017-08-30 00:44:52 +0000

    change path from committer to author

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
     i = 0
     while (i < 10):
         print("{}: {}".format(req[i].get('sha'),
-                              req[i].get('commit').get('committer').get('name')))
+                              req[i].get('commit').get('author').get('name')))
         i += 1
