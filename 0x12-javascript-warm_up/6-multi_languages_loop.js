f39ec69374f646ca4a1da0b050d8df9dc2a49151
kedpak <kedpak@yahoo.com>
2017-09-20 00:22:06 +0000

    first commit

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+let array = ['C', 'Python', 'Javascript'];
+let array2 = ['fun', 'cool', 'amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(array[i] + ' is ' + array2[i]);
+}
