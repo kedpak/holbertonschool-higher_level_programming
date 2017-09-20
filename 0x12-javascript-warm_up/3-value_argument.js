e0f55ba97930c34318e07a4c0d5163ff299d06ac
kedpak <kedpak@yahoo.com>
2017-09-20 04:42:13 +0000

    remove join method

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 let args = process.argv.slice(2);
-if (args[0] != undefined) {
-  console.log(args.join());
-} else {
+if (args[0] === null) {
   console.log('No argument');
+} else {
+  console.log(args[0]);
 }
