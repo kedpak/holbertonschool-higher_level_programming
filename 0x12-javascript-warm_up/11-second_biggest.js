e97a249dc299b2f07fb0dd4b588c84525734c96b
kedpak <kedpak@yahoo.com>
2017-09-20 04:25:19 +0000

    add condition if argument is none or one

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,20 +5,24 @@ let args = process.argv.slice(2);
 let max = Number(args[0]);
 let second = max;
 
-for (let i = 0; i < args.length; i++) {
-  if (Number(args[i]) > max) {
-    max = Number(args[i]);
+if (args.length === 0 || args.length === 1) {
+  second = 0;
+} else {
+  for (let i = 0; i < args.length; i++) {
+    if (Number(args[i]) > max) {
+      max = Number(args[i]);
+    }
   }
-}
-for (let j = 0; j < args.length; j++) {
-  if (Number(args[j]) === max) {
-    continue;
-  }
-  if (Number(args[0]) === max) {
-    second = Number(args[1]);
-  }
-  if (Number(args[j]) > second) {
-    second = Number(args[j]);
+  for (let j = 0; j < args.length; j++) {
+    if (Number(args[j]) === max) {
+      continue;
+    }
+    if (Number(args[0]) === max) {
+      second = Number(args[1]);
+    }
+    if (Number(args[j]) > second) {
+      second = Number(args[j]);
+    }
   }
 }
 console.log(second);
