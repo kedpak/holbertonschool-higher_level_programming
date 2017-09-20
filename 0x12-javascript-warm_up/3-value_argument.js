5cfc395702b624db0bea2afd1ae76dbfe3cc8cad
kedpak <kedpak@yahoo.com>
2017-09-20 04:43:47 +0000

    rearrange conditions

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 let args = process.argv.slice(2);
-if (args[0] === null) {
-  console.log('No argument');
-} else {
+if (args[0] != null) {
   console.log(args[0]);
+} else {
+  console.log('No argument');
 }
