e1e9f4e7948dbaedea0a609bee439241648a5451
kedpak <kedpak@yahoo.com>
2017-09-25 23:57:37 +0000

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Class defines a Rectangle
+// Takes width and height arguments
+// If w or h is empty, return empty object
+
+exports.Rectangle = function Rectangle (w, h) {
+  if (w === null || w <= 0 || h == null || h <= 0) {
+    return;
+  }
+  this.width = w;
+  this.height = h;
+};
