7764a53705e17e5580b84dec79d49cda19bd5b3b
kedpak <kedpak@yahoo.com>
2017-09-26 02:53:34 +0000

    use inheritance from number 5

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,26 +2,21 @@
 // Write a class Square that defines a square
 // and inherits from Rectangle.
 
-const Rectangle = require('./4-rectangle').Rectangle;
+const SquareInher = require('./5-square').Square;
 
 function Square (size) {
-  Rectangle.call(this, size, size);
+  SquareInher.call(this, size);
 }
 
-Square.prototype.charPrint = function (c) {
-  if (c == null) {
-    let rec = new Rectangle(this.width, this.height);
-    rec.print();
-  } else {
-    let list = [];
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
-        list.push(c);
-      }
-      let xPrint = list.join('');
-      console.log(xPrint);
-      list = [];
+Square.prototype.charPrint = function (c = 'X') {
+  let list = [];
+  for (let i = 0; i < this.width; i++) {
+    for (let j = 0; j < this.height; j++) {
+      list.push(c);
     }
+    let xPrint = list.join('');
+    console.log(xPrint);
+    list = [];
   }
 };
 
