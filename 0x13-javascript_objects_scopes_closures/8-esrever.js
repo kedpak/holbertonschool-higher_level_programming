ea455ad38fed2fcf54babbf60b7ee273d4ccf124
kedpak <kedpak@yahoo.com>
2017-09-26 03:30:00 +0000

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/urs/bin/node
+// Function that returns the reserved version of a list
+
+exports.esrever = function (list) {
+  let revList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    revList.push(list[i]);
+  }
+  return revList;
+};
