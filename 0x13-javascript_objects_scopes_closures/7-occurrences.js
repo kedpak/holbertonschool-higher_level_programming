fd22365b9045a2a4797ac2550f3ca6b04b950cd8
kedpak <kedpak@yahoo.com>
2017-09-26 03:20:57 +0000

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// count number of occurences inside list
+
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      counter += 1;
+    }
+  }
+  return counter;
+};
