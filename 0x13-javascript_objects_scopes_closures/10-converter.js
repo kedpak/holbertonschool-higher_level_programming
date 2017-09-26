c31a0ff3ebe1b21724a4e1fdeeeb16ade13a4b48
kedpak <kedpak@yahoo.com>
2017-09-26 06:47:26 +0000

    first commit

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+// Function that converts a number from base 10
+// to another base passed as argument
+
+exports.converter = function (base) {
+  return function (num) {
+    return parseInt(num, 10).toString(base);
+  };
+};
