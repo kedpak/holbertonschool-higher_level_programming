1e9a8eb6587f52412903734795250dc0f7a363fe
kedpak <kedpak@yahoo.com>
2017-09-26 20:59:44 +0000

    change var to let

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 /* Write a script that reads and prints the content of a file */
 
-var fs = require('fs');
+let fs = require('fs');
 let args = process.argv.slice(2);
 
-fs.readFile(args[0], 'utf8', function (err, data) {
+fs.readFile(args[0], 'utf-8', function (err, data) {
   if (err) {
-    throw err;
+    console.log(err);
   }
   console.log(data);
 });
