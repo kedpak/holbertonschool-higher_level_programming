4626137d756e9503da2e0c3e2a5f539af5e9313e
kedpak <kedpak@yahoo.com>
2017-09-26 00:10:29 +0000

    fix equal sign typo inside condition

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,7 +4,7 @@
 // If w or h is empty, return empty object
 
 exports.Rectangle = function Rectangle (w, h) {
-  if (w === null || w <= 0 || h == null || h <= 0) {
+  if (w === null || w <= 0 || h === null || h <= 0) {
     return;
   }
   this.width = w;
