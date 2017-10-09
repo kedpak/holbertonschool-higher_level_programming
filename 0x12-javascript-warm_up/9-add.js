266d637a6fc9832a180b17690a87dcda418ea180
kedpak <kedpak@yahoo.com>
2017-10-09 22:52:51 +0000

    update documentation

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-
+// Write a script that prints the addition of 2 integers
+// The first argument is the first integer, second arg second integer
 let args = process.argv.slice(2);
 
 function add (a, b) {
