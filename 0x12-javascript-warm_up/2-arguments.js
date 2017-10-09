272748ee3005c981f7948bb21bb1407fe9c67e9d
kedpak <kedpak@yahoo.com>
2017-10-09 22:47:24 +0000

    update documentation

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-
+//Write a script that prints a message depending of the number of arguments passed
 let args = process.argv.slice(2);
 if (args.length <= 0) {
   console.log('No argument');
