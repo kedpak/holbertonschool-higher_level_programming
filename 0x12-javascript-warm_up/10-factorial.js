fa2dabc95f7b9c3116fdb577a06e62dae484e93b
kedpak <kedpak@yahoo.com>
2017-10-09 22:53:46 +0000

    update documentation

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-
+// Write a script that computes and prints a factorial
+// The first argument is integer used for computing the factorial
+// Factorial of NaN is 1
 let args = process.argv.slice(2);
 
 function factorial (num) {
