6ce93346f787108b7bdcf3b4801965f82436d3c2
kedpak <kedpak@yahoo.com>
2017-10-09 22:48:11 +0000

    update documentation

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-
+// Write a script that prints the first argument passed to it
+// If no arguments are passed to the script, print “No argument”
 let args = process.argv.slice(2);
 if (args[0] != null) {
   console.log(args[0]);
