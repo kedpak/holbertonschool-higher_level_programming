087445845d4c49dca2973d72869b54960f1e7c86
kedpak <kedpak@yahoo.com>
2017-10-09 23:00:51 +0000

    update documentation

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
-
+// Write a class Rectangle that defines a rectangle
+// The constructor must take 2 arguments w and h
 exports.Rectangle = function Rectangle (w, h) {
   this.width = w;
   this.height = h;
